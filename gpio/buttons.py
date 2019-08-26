a09c251551b6849e843fe415a8c31f1f926648ac
Daniel <38507909+kodan50@users.noreply.github.com>
2019-08-26 15:53:59 -0700

    Update and rename gpio_buttons.py to buttons.py

--- gpio/buttons.py
+++ gpio/buttons.py
@@ -3,13 +3,13 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import time # Allows the use of delays to avoid output spamming and wasted CPU cycles
 import os # This code will allow us to run terminal commands
-GPIO.setmode(GPIO.BCM) #  Setting this to board and doing a pin count was my preferred stance, but it did not work and this did. So score a point for this method.
+GPIO.setmode(GPIO.BCM) # Just make sure this is set uniform to any other gpio scripts you run.
 
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set GPIO12 to be an input pin and set initial value to be pulled high (on?)
-GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set GPIO14 same as above.
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set GPIO12 to be an input pin and set initial value to be pulled high.
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set GPIO14 same as above.
 
 while True:
-	if GPIO.input(14) == 0:
+	if GPIO.input(4) == 0:
 		print('Raspberry Pi is going for a power down now!')
 		os.system("sudo shutdown -h now")
 	if GPIO.input(12) == 0:
